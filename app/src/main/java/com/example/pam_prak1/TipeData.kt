baf53bc0436f8de6cfc81ea1f10f39ed253bce82
iamanggi <anggipuspita452@gmail.com>
2024-10-11 20:25:28 +0700

    First Commit

---
+++ app/src/main/java/com/example/pam_prak1/TipeData.kt
@@ -0,0 +1,35 @@
+package com.example.pam_prak1
+
+fun main(){
+
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String ="Hello world!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("String: $stringValue")
+}
