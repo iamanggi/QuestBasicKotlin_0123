3749cdb0ddca1d6f6283d413861b9353b7970f5d
iamanggi <anggipuspita452@gmail.com>
2024-10-11 20:33:40 +0700

    membuat tipe collections Map

--- app/src/main/java/com/example/pam_prak1/Collections.kt
+++ app/src/main/java/com/example/pam_prak1/Collections.kt
@@ -67,3 +67,47 @@ fun ContohSet(){
     val shapesLocked: Set <String> = shape
     println(shapesLocked)
 }
+
+// Map adalah kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unordered, artinya data yang dimasukkan tidak memiliki poisisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+
+fun ContohMap(){
+    println()
+    println("=== Map ===")
+
+    //Map Read-Only
+    val readOnlyShape: Map<String, Int> =
+        mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    //Map mutable
+    val shape: MutableMap<String, Int> =
+        mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data ke dalam Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //Mengubah data didalam Map Mutable
+    shape["Square"]= 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main(){
+    ContohList()
+    ContohSet()
+    ContohMap()
+}
