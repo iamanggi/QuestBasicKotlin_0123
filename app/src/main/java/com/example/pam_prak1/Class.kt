4aeb15afeb90653b8b728f96ec2f11065e8c2f06
iamanggi <anggipuspita452@gmail.com>
2024-10-11 20:44:15 +0700

    membuat class Motor dan Contact dengan pembaruan email

--- app/src/main/java/com/example/pam_prak1/Class.kt
+++ app/src/main/java/com/example/pam_prak1/Class.kt
@@ -1,2 +1,18 @@
 package com.example.pam_prak1
 
+class Motor()
+
+class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Print the value of the property: email
+    println(contact.email)
+
+    //Update the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    //print the new value of the property: email
+    println(contact.email)
+}
